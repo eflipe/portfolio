008fb3cb5109aeeb2e3165ed1809e74d234d5c41
eflipe <felipecabaleiro@gmail.com>
2020-12-16 18:01:39 -0300

    Para github

---
+++ projects/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class ProjectModel(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+    techno = models.CharField(blank=True, max_length=100)
+    image = models.FilePathField(path="/img")  # , height_field=, width_field=
+    repo = models.URLField(blank=True)
+    sitio_web = models.URLField(blank=True)
---
+++ projects/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from projects.models import ProjectModel
+
+
+def project_index(request):
+    projects = ProjectModel.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+
+def project_detail(request, pk):
+    project = ProjectModel.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
